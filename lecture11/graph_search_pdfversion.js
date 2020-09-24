6b21302bd0ba7413f45e899a49687fdc9a83db4b
이민택 <imintaeg@imintaeg-ui-MacBookPro.local>
2020-09-24 10:10:25 +0900

    fix bfs,dfs

--- lecture11/graph_search_pdfversion.js
+++ lecture11/graph_search_pdfversion.js
@@ -18,6 +18,7 @@ class Graph {
     }
 
     print(){
+        console.log("\nmatrix:\n")
         let graphline = " ";
         let i, j;
         for (i = 0; i < this.matrix.length; i++) {
@@ -59,32 +60,32 @@ const bfs = (g, s) => {
         }
         color[u] = 'black';
     }
-    return {color,d,pi}
+    return {d,pi}
 }
+let time = 0;
 const dfs = (g) => {
     let color =[]
     for (let i = 0; i < g.length; i++) {
         color[i] = 'white';
     }
-    let time = 0;
     let d =[];
     let f = [];
     for (let i = 0; i < g.length; i++) {
         if(color[i]==='white'){
-            dfs_visit(i,g,color,time,d,f);
+            dfs_visit(i,g,color,d,f);
         }
     }
     return {d,f}
 }
 
-const dfs_visit = (u,g,color,time,d,f) => {
+const dfs_visit = (u,g,color,d,f) => {
     color[u] = 'gray';
     time += 1;
     d[u] = time;
     for (let i = 0; i < g.length; i++) {
-        if(g.matrix[u][i]){
+        if(g.matrix[u][i]===1){
             if(color[i]==='white'){
-                dfs_visit(u,g,color,time,d,f)
+                dfs_visit(i,g,color,d,f)
             }
         }
     }
@@ -92,28 +93,46 @@ const dfs_visit = (u,g,color,time,d,f) => {
     time +=1;
     f[u] = time;
 }
-let tempG = new Graph(9);
-tempG.insert(0,1);
-tempG.insert(0,2);
-tempG.insert(1,4);
-tempG.insert(2,4);
-tempG.insert(2,6);
-tempG.insert(3,1);
-tempG.insert(4,7);
-tempG.insert(5,2);
-tempG.insert(6,5);
-tempG.insert(6,7);
-tempG.insert(6,8);
-tempG.insert(7,6);
-tempG.insert(8,7);
+let bfsGraph = new Graph(9);
+bfsGraph.insert(0,1);
+bfsGraph.insert(0,2);
+bfsGraph.insert(1,4);
+bfsGraph.insert(2,4);
+bfsGraph.insert(2,6);
+bfsGraph.insert(3,1);
+bfsGraph.insert(4,3);
+bfsGraph.insert(4,7);
+bfsGraph.insert(5,2);
+bfsGraph.insert(6,5);
+bfsGraph.insert(6,7);
+bfsGraph.insert(6,8);
+bfsGraph.insert(7,6);
+bfsGraph.insert(8,7);
+bfsGraph.print();
 
-tempG.print();
+let dfsGraph = new Graph(8);
+dfsGraph.insert(0,1);
+dfsGraph.insert(0,3);
+dfsGraph.insert(0,4);
+dfsGraph.insert(1,2);
+dfsGraph.insert(1,3);
+dfsGraph.insert(2,0);
+dfsGraph.insert(4,3);
+dfsGraph.insert(4,5);
+dfsGraph.insert(5,3);
+dfsGraph.insert(6,4);
+dfsGraph.insert(6,5);
+dfsGraph.insert(6,7);
+dfsGraph.insert(7,3);
+dfsGraph.print();
 
-const bfs_result = bfs(tempG,0);
-const dfs_result = dfs(tempG,0);
+const bfs_result = bfs(bfsGraph,0);
+const dfs_result = dfs(dfsGraph);
 
+console.log('\nbfs result : ');
 console.dir(bfs_result);
-console.dir(dfs_result);
+console.log('\ndfs result : ');
+console.dir(dfs_result.d.map((value,index)=>[value,dfs_result.f[index]]));
 
 
 
